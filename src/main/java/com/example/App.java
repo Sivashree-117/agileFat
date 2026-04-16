dcf62d537c848a667b92b678eef4b38784c80678
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2026-04-16 14:21:07 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,20 +1,20 @@
 package com.example;
 
-public class App {
-
-    public static String calculateGrade(int marks) {
-
-        if (marks >= 90) return "A";
-        else if (marks >= 75) return "B";
-        else if (marks >= 60) return "C";
-        else if (marks >= 50) return "D";
-        else return "F";
-    }
-
+public class LibrarySystem {
     public static void main(String[] args) {
-        int marks = 85;
-        String grade = calculateGrade(marks);
-        System.out.println("Marks: " + marks);
-        System.out.println("Grade: " + grade);
+
+        boolean bookAvailable = true;
+        
+        if (bookAvailable) {
+            System.out.println("Book Issued");
+            bookAvailable = false;
+        } else {
+            System.out.println("Book Not Available");
+        }
+
+        bookAvailable = true;
+        System.out.println("Book Returned");
+
+        System.out.println("Available Now: " + bookAvailable);
     }
 }
