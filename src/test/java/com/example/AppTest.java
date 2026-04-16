4413683ec54eed353731da9800169c475b98ff27
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2026-04-16 14:25:59 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,17 +6,13 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 
     @Test
-    void testGradeA() {
-        assertEquals("A", App.calculateGrade(95));
+    void testavailableA() {
+        assertEquals("Issued", App.bookAvailable(true));
     }
 
     @Test
-    void testGradeB() {
-        assertEquals("B", App.calculateGrade(80));
+    void testavailableB() {
+        assertEquals("Not Issues", App.bookAvailable(false));
     }
 
-    @Test
-    void testGradeFail() {
-        assertEquals("F", App.calculateGrade(30));
-    }
 }
